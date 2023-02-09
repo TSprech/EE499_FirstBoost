95554c71d5d884af73e87d64637a7c3a498f3793
TSprech <trey.sprecher@gmail.com>
2023-02-09 12:29:55 -0500

    Python to Arduino Working
    Added to Py script to allow it to send data to the Arduino in JSON form and for it to be interpreted by the Pico.

--- STCpp.py
+++ STCpp.py
@@ -1,113 +1,24 @@
 import json
+from time import sleep
 import serial
 
 connection = serial.Serial(port="COM13")
-connection.reset_input_buffer()
+connection.reset_output_buffer()
 
-while True:
-    data = connection.readline().decode("utf-8")
-    # print(data)
-    try:
-        json_object = json.loads(data)
-        json_formatted_str = json.dumps(json_object, indent=2)
-        print(json_formatted_str)
-    except json.JSONDecodeError as e:
-        print("JSON:", e)
+# y = json.loads('{"LED":"1"}')
 
-# # Example Python Script
-# import time
-# from pySerialTransfer import pySerialTransfer as txfer
-#
-#
-# if __name__ == '__main__':
-#     try:
-#         link = txfer.SerialTransfer('COM5')
-#
-#         link.open()
-#         time.sleep(2) # allow some time for the Arduino to completely reset
-#
-#         while True:
-#             # send_size = 0
-#             #
-#             # ###################################################################
-#             # # Send a list
-#             # ###################################################################
-#             # list_ = [1, 3]
-#             # list_size = link.tx_obj(list_)
-#             # send_size += list_size
-#             #
-#             # ###################################################################
-#             # # Send a string
-#             # ###################################################################
-#             str_ = 'hello'
-#             # str_size = link.tx_obj(str_, send_size) - send_size
-#             # send_size += str_size
-#             #
-#             # ###################################################################
-#             # # Send a float
-#             # ###################################################################
-#             # float_ = 5.234
-#             # float_size = link.tx_obj(float_, send_size) - send_size
-#             # send_size += float_size
-#             #
-#             # ###################################################################
-#             # # Transmit all the data to send in a single packet
-#             # ###################################################################
-#             # link.send(send_size)
-#
-#             ###################################################################
-#             # Wait for a response and report any errors while receiving packets
-#             ###################################################################
-#             while not link.available():
-#                 if link.status < 0:
-#                     if link.status == txfer.CRC_ERROR:
-#                         print('ERROR: CRC_ERROR')
-#                     elif link.status == txfer.PAYLOAD_ERROR:
-#                         print('ERROR: PAYLOAD_ERROR')
-#                     elif link.status == txfer.STOP_BYTE_ERROR:
-#                         print('ERROR: STOP_BYTE_ERROR')
-#                     else:
-#                         print('ERROR: {}'.format(link.status))
-#
-#             # ###################################################################
-#             # # Parse response list
-#             # ###################################################################
-#             rec_size_ = link.rx_obj(obj_type=int,
-#                                     obj_byte_size=4)
-#
-#             ###################################################################
-#             # Parse response string
-#             ###################################################################
-#             rec_str_ = link.rx_obj(obj_type=str,
-#                                    start_pos=rec_size_,
-#                                    obj_byte_size=41)
-#
-#             # ###################################################################
-#             # # Parse response float
-#             # ###################################################################
-#             # rec_float_ = link.rx_obj(obj_type=type(float_),
-#             #                          obj_byte_size=float_size,
-#             #                          start_pos=(list_size + str_size))
-#             #
-#             # ###################################################################
-#             # # Display the received data
-#             # ###################################################################
-#             # print('SENT: {} {} {}'.format(list_, str_, float_))
-#             # print('RCVD: {} {} {}'.format(rec_list_, rec_str_, rec_float_))
-#             # print(' ')
-#             print(f'RCVD: Size:{rec_size_} Text:{rec_str_}')
-#
-#     except KeyboardInterrupt:
-#         try:
-#             link.close()
-#         except:
-#             pass
-#
-#     except:
-#         import traceback
-#         traceback.print_exc()
-#
-#         try:
-#             link.close()
-#         except:
-#             pass
+# connection.writelines(json.dumps(y))
+connection.write('{"LED":"1"}\r'.encode("ascii"))
+connection.flush()
+sleep(1)
+connection.write('{"LED":"0"}\r'.encode("ascii"))
+connection.flush()
+sleep(1)
+# data = connection.readline().decode("utf-8")
+# print(data)
+# try:
+#     json_object = json.loads(data)
+#     json_formatted_str = json.dumps(json_object, indent=2)
+#     print(json_formatted_str)
+# except json.JSONDecodeError as e:
+#     print("JSON:", e)
--- Src/main.cpp
+++ Src/main.cpp
@@ -13,49 +13,124 @@ using namespace nlohmann;
 // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
 constexpr float conversion_factor = 3.3f / (1 << 12);
 
+constexpr auto startLineLength = 8;
+
+/*
+ *  read a line of any  length from stdio (grows)
+ *
+ *  @param fullDuplex input will echo on entry (terminal mode) when false
+ *  @param linebreak defaults to "\n", but "\r" may be needed for terminals
+ *  @return entered line on heap - don't forget calling free() to get memory back
+ */
+static char * getLine(bool fullDuplex = true, char lineBreak = '\n') {
+  // th line buffer
+  // will allocated by pico_malloc module if <cstdlib> gets included
+  char * pStart = (char*)malloc(startLineLength);
+  char * pPos = pStart;  // next character position
+  size_t maxLen = startLineLength; // current max buffer size
+  size_t len = maxLen; // current max length
+  int c;
+
+  if(!pStart) {
+    return NULL; // out of memory or dysfunctional heap
+  }
+
+  while(1) {
+    c = getchar(); // expect next character entry
+    if(c == EOF || c == lineBreak) {
+      break;     // non blocking exit
+    }
+
+    if (fullDuplex) {
+      putchar(c); // echo for fullDuplex terminals
+    }
+
+    if(--len == 0) { // allow larger buffer
+      len = maxLen;
+      // double the current line buffer size
+      char *pNew  = (char*)realloc(pStart, maxLen *= 2);
+      if(!pNew) {
+        free(pStart);
+        return NULL; // out of memory abort
+      }
+      // fix pointer for new buffer
+      pPos = pNew + (pPos - pStart);
+      pStart = pNew;
+    }
+
+    // stop reading if lineBreak character entered
+    if((*pPos++ = c) == lineBreak) {
+      break;
+    }
+  }
+
+  *pPos = '\0';   // set string end mark
+  return pStart;
+}
+
 int main() {
   stdio_init_all();
 
-  adc_init();
-  adc_gpio_init(26);
-  adc_gpio_init(27);
-  adc_gpio_init(28);
-  adc_gpio_init(29);
+//  adc_init();
+//  adc_gpio_init(26);
+//  adc_gpio_init(27);
+//  adc_gpio_init(28);
+//  adc_gpio_init(29);
+//
+//  adc_select_input(0);
+//
+//  // Tell GPIO 0 and 1 they are allocated to the PWM
+//  gpio_set_function(0, GPIO_FUNC_PWM);
+//  gpio_set_function(1, GPIO_FUNC_PWM);
+//
+//  gpio_disable_pulls(0);
+//  gpio_disable_pulls(1);
+//
+//  gpio_set_slew_rate(0, GPIO_SLEW_RATE_FAST);
+//  gpio_set_slew_rate(1, GPIO_SLEW_RATE_FAST);
+//
+//  gpio_set_drive_strength(0, GPIO_DRIVE_STRENGTH_12MA);
+//  gpio_set_drive_strength(1, GPIO_DRIVE_STRENGTH_12MA);
+//
+//  // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
+//  auto slice = pwm_gpio_to_slice_num(0);
+//
+//  pwm_set_wrap(slice, 625 - 1);
+////  pwm_set_both_levels(slice, 5000, 1250);
+//  pwm_set_both_levels(slice, 295, 335);
+//  pwm_set_phase_correct(slice, true);
+//  pwm_set_output_polarity(slice, false, false);
+//  pwm_set_enabled(slice, true); // Start PWM running
 
-  adc_select_input(0);
-
-  // Tell GPIO 0 and 1 they are allocated to the PWM
-  gpio_set_function(0, GPIO_FUNC_PWM);
-  gpio_set_function(1, GPIO_FUNC_PWM);
-
-  gpio_disable_pulls(0);
-  gpio_disable_pulls(1);
-
-  gpio_set_slew_rate(0, GPIO_SLEW_RATE_FAST);
-  gpio_set_slew_rate(1, GPIO_SLEW_RATE_FAST);
-
-  gpio_set_drive_strength(0, GPIO_DRIVE_STRENGTH_12MA);
-  gpio_set_drive_strength(1, GPIO_DRIVE_STRENGTH_12MA);
-
-  // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
-  auto slice = pwm_gpio_to_slice_num(0);
-
-  pwm_set_wrap(slice, 625 - 1);
-//  pwm_set_both_levels(slice, 5000, 1250);
-  pwm_set_both_levels(slice, 295, 335);
-  pwm_set_phase_correct(slice, true);
-  pwm_set_output_polarity(slice, false, false);
-  pwm_set_enabled(slice, true); // Start PWM running
+  const uint32_t LED_PIN = PICO_DEFAULT_LED_PIN;
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
 
   json j;
+//  std::array<char, 200> s{};
 
   while(true) {
-    j["in"]["volts"] = 5.218852;
-    j["in"]["amps"] = 0.391453;
-    std::string json_str = j.dump();
+    char *pLine = getLine(true, '\r');
+    if (!*pLine) {
+      printf("returned empty - nothing blocked");
+    } else {
+      std::string_view s = std::string_view(pLine, strlen(pLine));
+      printf("entered: %s\r\n\r\n", pLine);
+      j = json::parse(s);
+      if (j["LED"] == "1") {
+        gpio_put(LED_PIN, true);
+      } else {
+        gpio_put(LED_PIN, false);
+      }
+      free(pLine); // dont forget freeing buffer !!
+    }
 
-    puts(json_str.data());
-
+//    j["in"]["volts"] = 5.218852;
+//    j["in"]["amps"] = 0.391453;
+//    std::string json_str = j.dump();
+//
+//    puts(json_str.data());
+//
     j.clear();
     sleep_ms(2000);
 
@@ -80,71 +155,76 @@ int main() {
 }
 
 
-
-
-
-//#include "pico/stdlib.h"
-//#include <cstdint>
-//#include <cstdio>
+//#include <stdio.h>
+//#include <cstdlib>          // need this include, if you need heap management (malloc, realloc, free)
+//#include <pico/stdlib.h>    // needed for Pico SDK support
 //
-//int main() {
-//  stdio_init_all();
-//  const uint32_t LED_PIN = PICO_DEFAULT_LED_PIN;
-//  gpio_init(LED_PIN);
-//  gpio_set_dir(LED_PIN, GPIO_OUT);
-//  while (true) {
-//    printf("Hello, World 2!\n");
-//    gpio_put(LED_PIN, true);
-//    sleep_ms(250);
-//    gpio_put(LED_PIN, false);
-//    sleep_ms(250);
+//const uint startLineLength = 8; // the linebuffer will automatically grow for longer lines
+//const char eof = 255;           // EOF in stdio.h -is -1, but getchar returns int 255 to avoid blocking
+//
+///*
+// *  read a line of any  length from stdio (grows)
+// *
+// *  @param fullDuplex input will echo on entry (terminal mode) when false
+// *  @param linebreak defaults to "\n", but "\r" may be needed for terminals
+// *  @return entered line on heap - don't forget calling free() to get memory back
+// */
+//static char * getLine(bool fullDuplex = true, char lineBreak = '\n') {
+//  // th line buffer
+//  // will allocated by pico_malloc module if <cstdlib> gets included
+//  char * pStart = (char*)malloc(startLineLength);
+//  char * pPos = pStart;  // next character position
+//  size_t maxLen = startLineLength; // current max buffer size
+//  size_t len = maxLen; // current max length
+//  int c;
+//
+//  if(!pStart) {
+//    return NULL; // out of memory or dysfunctional heap
 //  }
+//
+//  while(1) {
+//    c = getchar(); // expect next character entry
+//    if(c == eof || c == lineBreak) {
+//      break;     // non blocking exit
+//    }
+//
+//    if (fullDuplex) {
+//      putchar(c); // echo for fullDuplex terminals
+//    }
+//
+//    if(--len == 0) { // allow larger buffer
+//      len = maxLen;
+//      // double the current line buffer size
+//      char *pNew  = (char*)realloc(pStart, maxLen *= 2);
+//      if(!pNew) {
+//        free(pStart);
+//        return NULL; // out of memory abort
+//      }
+//      // fix pointer for new buffer
+//      pPos = pNew + (pPos - pStart);
+//      pStart = pNew;
+//    }
+//
+//    // stop reading if lineBreak character entered
+//    if((*pPos++ = c) == lineBreak) {
+//      break;
+//    }
+//  }
+//
+//  *pPos = '\0';   // set string end mark
+//  return pStart;
 //}
-
-
-//#include <cstdint>
-//#include <cstdio>
-//
-//#include "pico/stdlib.h"
-//#include "hardware/pwm.h"
-//#include "hardware/gpio.h"
-//
-//#include "json.hpp"
-//#include "SerialTransferCpp.hpp"
-//
-//using namespace nlohmann;
 //
 //int main() {
-//  stdio_init_all();
-//  json j;
+//    stdio_init_all(); // needed for redirect stdin/stdout to Pico's USB or UART ports
 //
-//  const uint32_t LED_PIN = PICO_DEFAULT_LED_PIN;
-//  gpio_init(LED_PIN);
-//  gpio_set_dir(LED_PIN, GPIO_OUT);
-//
-//  SerialConfig pico_config = {
-//        .put_c = [](auto ch) { putchar_raw(ch); },
-//        .get_c = []() -> int { return getchar(); },
-//        .peek_c = []() { return getchar_timeout_us(5) == PICO_ERROR_TIMEOUT; }};
-//
-//  stcpp::SerialTransfer stxfr;
-//  stxfr.begin(pico_config);
-//
-//  while(true) {
-//    gpio_put(LED_PIN, true);
-//    sleep_ms(1000);
-//    j["in"]["volts"] = 5.218852;
-//    j["in"]["amps"] = 0.391453;
-//    std::string json_str = j.dump();
-//
-//    puts(json_str.data());
-//    size_t send_size = 0;
-//    send_size = stxfr.txObj(static_cast<size_t>(json_str.length()), send_size);
-//    gpio_put(LED_PIN, false);
-//    send_size = stxfr.txObj(json_str.data(), send_size, json_str.length());
-//    stxfr.sendData(send_size);
-//
-//    j.clear();
-//    sleep_ms(1000);
-//  }
+//    while(1) {
+//        char *pLine = getLine(true, '\r');
+//        if (!*pLine) {
+//            printf("returned empty - nothing blocked");
+//        } else {
+//            printf("entered: %s\r\n\r\n", pLine);
+//            free(pLine); // dont forget freeing buffer !!
+//        }
+//    }
 //}
